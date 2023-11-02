9d0c81577b99ff64c5a11ddc13ce9f161a11b4f6
violanoronha <110602149+violanoronha@users.noreply.github.com>
2023-11-02 09:45:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add your JavaScript code here
+document.addEventListener("DOMContentLoaded", function() {
+    // You can add JavaScript functionality here
+    // For example:
+    // const header = document.querySelector("header");
+    // header.innerHTML = "Welcome to My Updated Website";
+});
